edd6a55824a010fa07bcf1f0e1f30bc704859b25
L1mmp <dima.wwe2@yandex.ru>
2022-07-12 12:06:25 +0300

    test cors

---
+++ TestWebApi/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestController : ControllerBase
+	{
+		[HttpGet]
+		[Route("get")]
+		public string GetPage()
+		{
+			Response.Headers.Append("Access-Control-Allow-Origin", "*");
+			return "qwe";
+		}
+	}
+}
